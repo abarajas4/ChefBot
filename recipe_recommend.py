6329c1d80741dd5dadc2e04532622cc73d89fe26
abarajas04 <andrebarajas2000@gmail.com>
2024-06-25 14:19:39 -0500

    added recipe and gpt files

---
+++ recipe_recommend.py
@@ -0,0 +1,12 @@
+import requests 
+import json
+
+
+API_KEY = "de9a68661df04656bf8240a057bbf72a"
+url = f'https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey={API_KEY}&number=2'
+
+response = requests.get(url)
+data = response.json()
+
+for recipe in data["results"]:
+    print(recipe["title"])
