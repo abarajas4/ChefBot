a1601334305fa0c441e29a8cc3db0f858ce68094
abarajas04 <andrebarajas2000@gmail.com>
2024-06-26 12:50:43 -0500

    edits to recipe

--- recipe_recommend.py
+++ recipe_recommend.py
@@ -2,13 +2,15 @@ import requests
 import json
 
 
-API_KEY = ""
-url = f'https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey={API_KEY}&number=2'
+API_KEY = "de9a68661df04656bf8240a057bbf72a"
+url = f"https://api.spoonacular.com/recipes/complexSearch?apiKey={API_KEY}" \
+"&number=1&cuisine=American&addRecipeInstructions=True&instructionsRequired=True&addRecipeInformation=True"
 
 response = requests.get(url)
 data = response.json()
+print(data)
 
-for recipe in data["results"]:
-    print(recipe["title"])
+#for recipe in data['results']:
+   #print(recipe["title"])
 
 #testing if Yuneydy can push
