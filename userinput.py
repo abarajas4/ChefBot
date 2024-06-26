e20a9c0d2012dfe2fe99caa5726dcc112eacbb63
abarajas04 <andrebarajas2000@gmail.com>
2024-06-26 12:34:57 -0500

    added get cuisine

--- userinput.py
+++ userinput.py
@@ -21,6 +21,7 @@ def get_dietary_restrictions():
     selected_restriction = []
     initial = input("Do you have any dietary restrictions? y/n: ")
     initial.lower()
+
     if initial == "n": 
         print ("\n You have selected no dietary restrictions")
         return selected_restriction
@@ -34,9 +35,76 @@ If you have more than one diet, please separate the values with a comma\n""")
         for restriction in restrictions:
             if restriction.strip() in options: 
                 selected_restriction.append(restriction.strip())
+
     print("\nYou have selected the following dietary restrictions:")
+
     for res in selected_restriction:
         print(res)
+
+    mistake = input("\nAre these restrictions correct? y/n: ")
+    mistake.lower()
+
+    if mistake == "n":
+        get_dietary_restrictions()
+
     return selected_restriction
 
+def get_cuisines():
+    options = {
+        "African", 
+        "Asian", 
+        "American", 
+        "British", 
+        "Cajun", 
+        "Carribean", 
+        "Chinese", 
+        "Eastern European", 
+        "European", 
+        "French", 
+        "German", 
+        "Greek", 
+        "Indian", 
+        "Irish", 
+        "Italian", 
+        "Japanese", 
+        "Korean",
+        "Latin American", 
+        "Mediterranean", 
+        "Mexican", 
+        "Middle Eastern", 
+        "Nordic", 
+        "Southern", 
+        "Spanish", 
+        "Thai", 
+        "Vietnamese"
+    }
+
+    selected_cuisines = []
+    print("""\nDo you have any preferred cuisines?
+Enter as many as you like from the list below by separating names with a comma\n""")
+    
+    for option in options: 
+        print(option)
+
+    user_input = input("\nEnter your preferred cuisines: ")
+    cuisines = user_input.split(",")
+
+    for cuisine in cuisines:
+        if cuisine.strip() in options:
+            selected_cuisines.append(cuisine.strip())
+    print("\nYou have selected the following preferred cuisines:\n")
+
+    for cuisine in selected_cuisines:
+        print(cuisine)
+
+    mistake = input("\nAre these correct? y/n: ")
+    mistake.lower()
+
+    if mistake == "n":
+        get_cuisines()
+
+    return selected_cuisines
+
+
 get_dietary_restrictions()
+get_cuisines()
