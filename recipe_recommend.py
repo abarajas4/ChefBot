082d907a32ec93a7ef5d1a9bfd8b5a9c8ce7b667
abarajas04 <andrebarajas2000@gmail.com>
2024-06-27 21:42:00 -0500

    added pep8 style

--- recipe_recommend.py
+++ recipe_recommend.py
@@ -4,15 +4,15 @@ import pandas as pd
 
 
 def recipe_list(data):
-   result = []
-   for recipe in data['results']:
-      title = recipe["title"]
-      source_url = recipe["sourceUrl"]
-      serving_price = (recipe["pricePerServing"] / 100)
-      servings = recipe["servings"]
-      totalPrice = round(serving_price * servings, 2)
-      result.append([title, totalPrice, source_url])
-   return result
+    result = []
+    for recipe in data['results']:
+        title = recipe["title"]
+        source_url = recipe["sourceUrl"]
+        serving_price = (recipe["pricePerServing"] / 100)
+        servings = recipe["servings"]
+        totalPrice = round(serving_price * servings, 2)
+        result.append([title, totalPrice, source_url])
+    return result
 
 
 def main():
@@ -22,24 +22,26 @@ def main():
     meals = userinput.get_num_of_meals()
     serving_size = userinput.get_serving_size()
 
-    API_KEY = "de9a68661df04656bf8240a057bbf72a"
-    url = f"https://api.spoonacular.com/recipes/complexSearch?apiKey={API_KEY}" \
-          f"&number={meals}&cuisine={cuisines}&diet={dietary}&type={meal_type}" \
-          f"&minServings={serving_size}&addRecipeInformation=True"
+    A_KEY = "de9a68661df04656bf8240a057bbf72a"
+    url = f"https://api.spoonacular.com/recipes/complexSearch?apiKey={A_KEY}" \
+          f"&number={meals}&cuisine={cuisines}&diet={dietary}" \
+          f"&type={meal_type}&minServings={serving_size}" \
+          f"&addRecipeInformation=True"
 
     response = requests.get(url)
     data = response.json()
 
     recipes = recipe_list(data)
 
-    print("\nWith your inputs in mind, here is your ChefBot approved Meal Plan: \n")
+    print("\nHere is your personalized, ChefBot approved Meal Plan: \n")
 
     for i, recipe in enumerate(recipes, 1):
         result = ""
         title, price, source_url = recipe
         print(f"\nRecipe {i}: {title}")
         print(f"    Total Price of Recipe: ${price}")
-        sen = "Learn more about the recipe, including ingredients and instructions for preparation"
+        sen = """Learn more about the recipe,
+        including ingredients and instructions for preparation"""
         hyperlink = f"\033]8;;{source_url}\a{sen}\033]8;;\a"
         result += hyperlink + "\n"
         print(f"    {result}")
@@ -55,19 +57,20 @@ def main():
 
 
 if __name__ == "__main__":
-    print("\nWelcome to ChefBot, the #1 program for designing your personal meal plan\n")
+    print("\nWelcome to ChefBot!\n")
+    print("We are the #1 program for designing a personal meal plan\n")
     print("So how does ChefBot work?\n")
-    print("""     Chefbot will ask you a range of questions, from your dietary restrictions, 
-     to your preferred cuisines, and even how many meals you would like in your meal plan\n""")
-    print("""     By giving ChefBot the most accurate information about yourself, we can tailor
-     your mealplan directly to your wants and needs\n""")
-    print("""     Along with the name of a diverse set of dishes, ChefBot will include information
-     on the price, ingredients, and preparation instructions to ensure any recipe on your
-     meal plan is accessible and easy to follow along\n""")
-    print("""     Through ChefBot, we want to make cooking easier for all students to avoid any nightly 
-     ramen or otherwise plain meals. Let's make cooking fun!\n""")
+    print("  Chefbot will ask questions about your dietary restrictions,")
+    print("  preferred cuisines, and how many meals you'd like in your plan\n")
+    print("  By giving ChefBot the most accurate information, we can tailor")
+    print("  your mealplan directly to your wants and needs\n")
+    print("  Along with the recipes, ChefBot will include information")
+    print("  on the price, ingredients, and preparation instructions\n")
+    print("  This ensures meal plans are easy to follow along\n")
+    print("  Through ChefBot, we want to make cooking easier.")
+    print("  Let's make cooking fun!\n")
 
-    user_input = input("How does that sound? Are you ready to get started with ChefBot? y/n: ")
+    user_input = input("Are you ready to get started with ChefBot? y/n: ")
     if user_input.lower() == "y":
         main()
     else:
